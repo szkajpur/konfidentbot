04279c97ba7dcad5999ffa4be485459153c4ae3d
szkajpur <70982115+szkajpur@users.noreply.github.com>
2022-02-01 23:30:26 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+`use strict`;
+
+const botUsername = "";
+const botOauth = ""; // without "oauth:"
+const prefix = "!";
+const userCooldown = "5000"; // 5 seconds
+const globalCooldown = "2000" // 2 seconds
+const ownerID = ""; // owner id
+const botID = ""; // bot account id 
+const connectChannels = [
+    'pajlada'
+];
+
+module.exports = {
+    botUsername,
+    botOauth,
+    prefix,
+    userCooldown,
+    globalCooldown,
+    ownerID,
+    botID,
+    connectChannels
+};
