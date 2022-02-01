d3289470fda9ef76de69e3877f2a33f334551729
szkajpur <70982115+szkajpur@users.noreply.github.com>
2022-02-01 23:31:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,234 +1,233 @@
-const config = require('./config.json');
-const commands = require('./commands.json');
-const { ChatClient } = require('dank-twitch-irc');
+const { ChatClient, AlternateMessageModifier, SlowModeRateLimiter } = require('dank-twitch-irc');
+const chalk = require('chalk');
 const got = require('got');
-const moment = require('moment');
-const usernameRegex = new RegExp(/^@?([\w]{1,25}),?$/);
-var cooldown = false;
-// Helper functions
+const config = require('./config');
 
-// Declare clients
+const usernameRegex = new RegExp(/^@?([\w]{1,25}),?$/);
+
+const talkedRecently = new Set();
+let userCD = parseInt(config.userCooldown);
+let globalCD = parseInt(config.globalCooldown);
+var cooldown = false;
+
+// declare client
 let client = new ChatClient({
     username: config.botUsername,
     password: `oauth:${config.botOauth}`,
     rateLimits: "default"
 });
-let clientRead = new ChatClient({
-    username: "justinfan12345",
-    rateLimits: "verifiedBot"
-});
 
 
-// Events on client
-client.on("connecting", () => {
-    console.log(`Connecting ${config.botUsername} to #${config.channel} twitch chat...`);
-})
-client.on("ready", () => {
-    console.log(`Successfully connected ${config.botUsername} #${config.channel} twitch chat!`);
-	client.say(config.channel, `Successfully connected ${config.botUsername} to #${config.channel} channel!`);
-})
-client.on("close", (error) => {
-    if (error != null) {
-        console.error('Client closed due to error: ', error);
-    } else {
-        console.error('Client closed due to unknown error!');
-    }
-})
-client.on("error", (error) => {
-    if (error != null) {
-        console.error('Client error: ', error);
-    } else {
-        console.error('Client unknown error!');
-    }    
-})
-
-// Events on clientRead
-clientRead.on("connecting", () => {
-    console.log("Read | Connecting...");
+// events on client
+client.use(new AlternateMessageModifier(client));
+client.use(new SlowModeRateLimiter(client, 10));
+client.on("ready", async () => {
+	console.log(`Successfully connected to chat`);
 });
-clientRead.on("ready", () => {
-    console.log("Read | Connected!");
-    clientRead.join("mamm0n");
-});
-clientRead.on("close", (error) => {
-    if (error != null) {
-        console.error("Read | Client closed due to error", error);
+client.on("close", async (error) => {
+    if (error !== null){
+        console.error(`Client closed due to error`, error);
     }
 });
 
-// sayMulti sends the message in multiple channels at once through client specified
-function sayMulti(client, channels, message) {
-    for (channel of channels) {
-        client.say(channel, message).catch(console.error);
-    }
-}
-
-// cooldown function
-function cd(time){
-    cooldown = true;
-    setTimeout(() => {
-        cooldown = false;
-    }, parseInt(time));
-}
-
-//cytat function
-function cytat(nick){
+// Cytat command function
+function cytat(channel, username){
     (async () => {
         try {
             do {
-                var response = await got(`https://harambelogs.pl/channel/demonzz1/user/${nick}/random`);
+                var response = await got(`https://harambelogs.pl/channel/demonzz1/user/${username}/random`);
             } while (!response.body.includes('#demonzz1'));
-            if (nick == 'pierdzibak_kwiatka' || nick == 'pierdzibak_laskawy') {
+            if (username == 'pierdzibak_kwiatka' || username == 'pierdzibak_laskawy') {
                 var emote = "Pierdzibak";
             } else
-            if (nick == 'ltk__') {
+            if (username == 'ltk__') {
                 var emote = "Madge";
             } else
-            if (nick == 'amazingniepolak') {
-                var emote = "demonzPe";
-            } else
-            if (nick == 'julkalols') {
+            if (username == 'julkalols') {
                 var emote = "mitoman";
             }
-            if (nick == 'gawcio69') {
+            if (username == 'gawcio69') {
                 var emote = "peepoFoil";
             }
-            let wiadomosc = response.body;
-            if (wiadomosc.includes(nick)){
-                let quote = wiadomosc.split(`#demonzz1 ${nick}:`);
-                let cytat = quote[1].trim();
-                client.say(config.channel, `${emote} Thinking ${cytat} Thinking2 ${quote[0]}`);
-                cd(5000);
+            let bodyCytat = response.body;
+            if (bodyCytat.includes(username)){
+                let quote = bodyCytat.split(`#demonzz1 ${username}:`);
+                let quoteMsg = quote[1].trim();
+                client.say(channel, `${emote} Thinking ${quoteMsg} Thinking2 ${quote[0]}`);
                 return;
-            } else if (wiadomosc.includes('pierdzibak_laskawy')) {
-                let quote = wiadomosc.split(`#demonzz1 pierdzibak_laskawy:`);
-                let cytat = quote[1].trim();
-                client.say(config.channel, `${emote} Thinking ${cytat} Thinking2 ${quote[0]}`);
-                cd(5000);
+            } else if (bodyCytat.includes('pierdzibak_laskawy')) {
+                let quote = bodyCytat.split(`#demonzz1 pierdzibak_laskawy:`);
+                let quoteMsg = quote[1].trim();
+                client.say(channel, `${emote} Thinking ${quoteMsg} Thinking2 ${quote[0]}`);
                 return;    
             } else {
-                client.say(config.channel, `Nie udało się pobrać cytatu PoroSad`);
-                cd(3000);
+                client.say(channel, `Nie udało się pobrać cytatu PoroSad`);
                 return;
             }
         } catch (error) {
-            console.log(error.response);
-            cd(3000);
+            console.error(error.response);
             return;
         }
     })();
+};
+
+function checkUsername(toCheck){
+    if (toCheck.length < 1){
+        return null;
+    }
+    let match = usernameRegex.exec(toCheck[0].toLowerCase());
+    if (match === null){
+        return null;
+    }
+    let target = match[1];
+    return target;
 }
 
-clientRead.on("JOIN", (msg) => {
-    console.log(`Read | Joined #${msg.channelName}`);
-});
-
-clientRead.on("CLEARCHAT", (msg) => {
-    if (msg.isPermaban()) {
-        sayMulti(client, config.channel, `⚠ ${msg.targetUsername} dostał perma u mamona! mamon ⚠`);
-    }
-});
-
 client.on("PRIVMSG", async (msg) => {
-    if (msg.senderUsername == client.configuration.username){
+    if (msg.senderUserID === config.botID){
         return;
-    }
+    };
     if (!msg.messageText.startsWith(config.prefix)){
         return;
-    }
-    if (cooldown == true){
+    };
+    if ((cooldown == true) && !(msg.senderUserID === config.ownerID)){
+        console.log(`User Cooldown`); 
         return;
     }
-    let args = msg.messageText.slice(config.prefix.length).trim().split(/\s+/);
-    const command = args.shift().toLowerCase();
+    if (talkedRecently.has(msg.senderUserID) && !(msg.senderUserID === config.ownerID)){
+        console.log(`Global Cooldown`);
+        return;
+    };
+    let args = msg.messageText.slice(config.prefix.length).trim().split(/\s+/); // object 
+    const command = args.shift().toLowerCase(); // string ONLY command
+    const stripPrefix = String(msg.messageText).substring(config.prefix.length); // the same as "args" but this is STRING
     switch(command){
+        case 'ping':
+            client.say(msg.channelName, `PONG FeelsDankMan`);
+            break;
         case 'help':
-            client.say(config.channel, `@${msg.senderUsername}, ${commands.help}`);
-            cd(3000);
+            client.say(msg.channelName, `@${msg.senderUsername}, dostępne komendy: ${config.prefix}ping, ${config.prefix}help, ${config.prefix}czyjpies [nick], ${config.prefix}top5 [nick], ${config.prefix}lastseen [nick], ${config.prefix}cytat_pierdzibaka, ${config.prefix}cytat_janusza, ${config.prefix}cytat_ltk, ${config.prefix}cytat_gawcia FeelsDankMan`);
+            break;
+        case 'echo':
+            if (msg.senderUserID === config.ownerID){
+                let echoMsg = stripPrefix.replace(/^echo/gi,``);
+                if (!echoMsg.length){
+                    client.say(msg.channelName, `@${msg.senderUsername}, podaj wiadomość do pokazania! FeelsDankMan`);
+                    return;
+                } else {
+                    client.me(msg.channelName, `FeelsDankMan 📣 ${echoMsg}`);
+                };
+            };
+            break;
+        case 'spam':
+            if (parseInt(args[0]) == null){
+                client.say(msg.channelName, `@${msg.senderUsername}, ${config.prefix}spam [ilość] [wiadomość] NaM`);
+                return;
+            }
+            let spamTimes = parseInt(args[0]);
+            let spamText = String(args.slice(1).join(' '));
+            if (!spamText.length){
+                client.say(msg.channelName, `@${msg.senderUsername}, ${config.prefix}spam [ilość] [wiadomość] NaM`);
+                return;
+            };
+            while (spamTimes > 0){
+                client.say(msg.channelName, spamText);
+                spamTimes = spamTimes - 1;
+            };
             break;
         case 'czyjpies':
-            if (args.length < 1){
-                client.say(config.channel, `@${msg.senderUsername}, podaj nick!`);
-                cd(3000);
+            let target = checkUsername(args);
+            if (target == null){
+                client.say(msg.channelName, `@${msg.senderUsername}, ${config.prefix}czyjpies [nick] ppL`);
                 return;
-            }
-            let match = usernameRegex.exec(args[0].toLowerCase());
-            if (match === null){
-                client.say(config.channel, `@${msg.senderUsername}, podaj poprawny nick!`);
-                cd(3000);
-                return;
-            }
-            let target = match[1];
+            };
             (async () => {
                 try {
                     const response = await got(`https://xayo.pl/api/watchtime/${target}`);
-                    const lista = JSON.parse(response.body);
-                    if (!lista.length) {
-                        client.say(config.channel, `@${msg.senderUsername}, nie ma podanego użytkownika w bazie danych!`);
-                        cd(3000);
+                    const list = JSON.parse(response.body);
+                    if (!list.length) {
+                        client.say(msg.channelName, `@${msg.senderUsername}, nie ma podanego użytkownika w bazie danych. PoroSad`);
                         return;
                     }
-                    const bruh = `${lista[0].streamer} (${lista[0].count}m)`;
-                    client.say(config.channel, `@${msg.senderUsername}, ${target} jest psem: ${bruh} Wowee`);
+                    let message = `${list[0].streamer} (${list[0].count}min)`;
+                    client.say(msg.channelName, `@${msg.senderUsername}, ${target} jest psem: ${message} Wowee`);
                 } catch (error) {
-                    console.log(error.response);
-                    cd(3000);
+                    console.error(error.response);
                     return;
                 }
             })();
-            cd(5000);
             break;
         case 'top5':
-            if (args.length < 1){
-                client.say(config.channel, `@${msg.senderUsername}, podaj nick!`);
-                cd(3000);
+            let target2 = checkUsername(args);
+            if (target2 == null){
+                client.say(msg.channelName, `@${msg.senderUsername}, ${config.prefix}top5 [nick] ppL`);
                 return;
-            }
-            let pagman = usernameRegex.exec(args[0].toLowerCase());
-            if (pagman === null){
-                client.say(config.channel, `@${msg.senderUsername}, podaj poprawny nick!`);
-                cd(3000);
-                return;
-            }
-            let okge = pagman[1];
+            };
             (async () => {
                 try {
-                    const response = await got(`https://xayo.pl/api/watchtime/${okge}`);
-                    const lista = JSON.parse(response.body);
-                    if (!lista.length) {
-                        client.say(config.channel, `@${msg.senderUsername}, nie ma podanego użytkownika w bazie danych!`);
-                        cd(3000);
+                    const response = await got(`https://xayo.pl/api/watchtime/${target2}`);
+                    const list = JSON.parse(response.body);
+                    if (!list.length) {
+                        client.say(msg.channelName, `@${msg.senderUsername}, nie ma podanego użytkownika w bazie danych. PoroSad`);
                         return;
                     }
-                    const bruh = `${lista[0].streamer} ${lista[0].count}m | ${lista[1].streamer} ${lista[1].count}m | ${lista[2].streamer} ${lista[2].count}m | ${lista[3].streamer} ${lista[3].count}m | ${lista[4].streamer} ${lista[4].count}m`;
-                    client.say(config.channel, `@${msg.senderUsername}, TOP5 ${okge} -> ${bruh} PepoG`);
+                    let message = `${list[0].streamer} ${list[0].count}min | ${list[1].streamer} ${list[1].count}min | ${list[2].streamer} ${list[2].count}min | ${list[3].streamer} ${list[3].count}min | ${list[4].streamer} ${list[4].count}min`;
+                    client.say(msg.channelName, `@${msg.senderUsername}, TOP5 ${target2} -> ${message} PepoG`);
                 } catch (error) {
-                    console.log(error.response);
-                    cd(3000);
+                    console.error(error.response);
                     return;
                 }
             })();
-            cd(5000);
+            break;
+        case 'lastseen':
+            let target3 = checkUsername(args);
+            if (target3 == null){
+                client.say(msg.channelName, `@${msg.senderUsername}, ${config.prefix}lastseen [nick] ppL`);
+                return;
+            };
+            (async () => {
+                try {
+                    const response = await got(`https://vislaud.com/api/chatters?logins=${target3}`);
+                    const list = JSON.parse(response.body);
+                    if (list[0] == null) {
+                        client.say(msg.channelName, `@${msg.senderUsername}, nie ma podanego użytkownika w bazie danych. PoroSad`);
+                        return;
+                    }
+                    let streamer = list[0].lastseen.streamer.displayName;
+                    let lastseen = list[0].lastseen.timestamp;
+                    lastseen = lastseen.split('T');
+                    let lastseenDate = String(lastseen[0]);
+                    let lastseenTime = new Date(lastseen).toLocaleTimeString('pl-PL');
+                    client.say(msg.channelName, `@${msg.senderUsername}, użytkownik ${target3} był ostatnio widzany na kanale: ${streamer} (${lastseenTime} ${lastseenDate})`);
+                } catch (error) {
+                    console.error(error.response);
+                    return;
+                }
+            })();  
             break;
         case 'cytat_pierdzibaka':
-            cytat('pierdzibak_kwiatka');
-            break;
-        case 'cytat_amazinga':
-            cytat('amazingniepolak');
+            cytat(msg.channelName, 'pierdzibak_kwiatka');
             break;
         case 'cytat_janusza':
-            cytat('julkalols');
+            cytat(msg.channelName, 'julkalols');
             break;
         case 'cytat_ltk':
-            cytat('ltk__');
+            cytat(msg.channelName, 'ltk__');
             break;
         case 'cytat_gawcia':
-            cytat('gawcio69');
+            cytat(msg.channelName, 'gawcio69');
             break;
     }
-}
-);
+    talkedRecently.add(msg.senderUserID);
+    setTimeout(() => {
+        talkedRecently.delete(msg.senderUserID);       
+    }, userCD);
+
+    cooldown = true;
+    setTimeout(() => {
+        cooldown = false;
+    }, globalCD);
+});
 
 client.connect();
-client.join(config.channel);
-clientRead.connect();
+client.joinAll(config.connectChannels);
